81f7568da29b9e74936e20c5a8e1275c1a529c5c
Prakharprasun <prakharprasun004@gmail.com>
2024-09-29 20:28:55 +0530

    initial commit

---
+++ A.cpp
@@ -0,0 +1,14 @@
+//By Prakhar Prasun
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	
+	cout << "ans" << "\n";
+}
