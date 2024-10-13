8c0ed366b5af0ab14c931ec0cfe058687a2e0ab4
Prakharprasun <prakharprasun004@gmail.com>
2024-10-13 19:25:43 +0530

    added sorting

---
+++ Techniques/Precomputation/README.md
@@ -0,0 +1,7 @@
+# Precomputation
+
+## Hashing + PrefixSum
+
+A very intresting technique when it comes to precomputation is:
+
+setting a[l] = x and a[r+1] = -x can make exery element in range l-r, x
---
+++ Techniques/Precomputation/Recursion/Factorial_Recursion.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Factorial(int n){
+    if(n==0) return 1;
+    return (n*Factorial(n-1));
+}
+
+int main() {
+    int n = 3;
+    cout << Factorial(n);
+}
