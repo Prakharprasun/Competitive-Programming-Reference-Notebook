9c6f378d6a78c3e807ccca89bf85f8052616c22f
Prakharprasun <prakharprasun004@gmail.com>
2024-11-02 13:54:04 +0530

    added precompiled stdc++.h

--- cses.cpp
+++ cses.cpp
@@ -17,27 +17,29 @@ int32_t main() {
     int t;
     cin >> t;
     while (t--){
+
         int n;
         cin >> n;
-        vector<int> a;
-        for(int i=1;i<=n;i++){
-            int x;
-            cin >> x;
-            a.push_back(x);
+        int ans = 0;
+        vector<int> a(n);
+        map<int,int> m;
+        for(auto &i : a){
+            cin >> i;
         }
-        int ans = -1;
-        int min = *min_element(a.begin(),a.end());
-        int ma = *max_element(a.begin(),a.end());
-        ans = max(a[n-1]-min,ans);
-        ans = max(ma-a[0],ans);
-        vector<int> b(2*n);
-        for(int i=0;i<2*n;i++){
-            b[i] = a[i%n];
+        int ma = -1;
+
+        for(auto i : a){
+            m[i]++;
         }
-        for(int i=0;i<n;i++){
-            ans = max(ans, b[i+n-1] - b [i]);
+        for(auto i : m){
+            ma = max(ma,i.s);
         }
-        cout << ans << '\n';
+        
+        ans = (ceil(log2(ceil((double)n/ma))));
+        //ans = ceil((n-ma)/ma);
+        ans += (n-ma);
+
+         cout << ans << '\n';
     } 
 }
 
